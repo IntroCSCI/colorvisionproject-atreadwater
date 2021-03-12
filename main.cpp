f322e154455b824ee762dff103bb66e1d7af17ef
Andrew Treadway <astreadway@mail.csuchico.edu>
2021-03-12 12:06:09 +0000

    the codes

--- main.cpp
+++ main.cpp
@@ -1,25 +1,30 @@
-//Author: PUT YOUR NAME HERE
+//Author: Andrew Treadway
 #include <iostream>
-#include "libraries/bitmap.h"
+#include "libraries/bitmap(1).h"
 #include <fstream> 
 using namespace std;
 
 string filename;
 fstream reader;
+string color = "";
 
 int main()
 {
-
-
-cout << "Enter a file name for analysis.\n";
+cout << "Enter a file name for color pallete analysis.\n";
 cin >> filename;
 
-reader.open(filename, ios::in);
+reader.open (filename, ios::in);
   if ( reader.is_open() ) {
     cout << " File has opened.\n";
-  }
+  
+  while ( !reader.eof() ); 
+    getline(reader,color);
+      // insert code for hexadecimal rgb 
+      cout << "The colors found in the file are: " << color << endl;
 
-
+      reader.close(); } 
+  else 
+    cout << "File could not be opened.\n";
 
   return 0;
 }
